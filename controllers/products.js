cb44f4f1148f1fd981fbbc96dbefd18c9d66ef6d
Youssef-hilaly <yousif.ossama2000@gmail.com>
2024-04-16 10:17:23 +0200

    adding readme.md file content and delete testing route

--- controllers/products.js
+++ controllers/products.js
@@ -1,11 +1,5 @@
 const Product = require('../models/product');
 
-const getAllProductsStatic = async (req, res) => {
-    const products = await Product.find({});
-
-    res.status(200).json({ products, nbHits: products.length });
-}
-
 const getAllProducts = async (req, res) => {
     const { featured, company, name, sort, fields, numericFilters } = req.query;
     const queryObj = {};
@@ -14,45 +8,45 @@ const getAllProducts = async (req, res) => {
         queryObj.featured = featured === 'true' ? true : false;
     }
 
-    if(company){
-        queryObj.company = company ;
+    if (company) {
+        queryObj.company = company;
     }
 
-    if(name){
-        queryObj.name = { $regex: name, $options:'i' }
+    if (name) {
+        queryObj.name = { $regex: name, $options: 'i' }
     }
 
-    if(numericFilters){
+    if (numericFilters) {
         const operatorMap = {
-            '>' : '$gt',
-            '>=' : '$gte',
-            '<' : '$lt',
-            '<=' : '$lte',
-            '=' : '$eq',
+            '>': '$gt',
+            '>=': '$gte',
+            '<': '$lt',
+            '<=': '$lte',
+            '=': '$eq',
         }
         const reqEx = /\b(>|>=|=|<=|<)\b/g
-        
-        let filters = numericFilters.replace(reqEx, (match)=>`-${operatorMap[match]}-`);
+
+        let filters = numericFilters.replace(reqEx, (match) => `-${operatorMap[match]}-`);
         const options = ['price', 'rating'];
         filters = filters.split(',').forEach(element => {
             const [field, operator, value] = element.split('-');
             // console.log(field, operator, value);
-            if(options.includes(field)){
-                queryObj[field] = {[operator]: Number(value)};
+            if (options.includes(field)) {
+                queryObj[field] = { [operator]: Number(value) };
             }
         });
     }
 
-    let result =  Product.find(queryObj);
-    
-    if(sort){
+    let result = Product.find(queryObj);
+
+    if (sort) {
         const sortList = sort.split(',').join(' ');
         result = result.sort(sortList);
-    }else{
+    } else {
         result = result.sort('createdAt');
     }
 
-    if(fields){
+    if (fields) {
         const fieldList = fields.split(',').join(' ');
         result = result.select(fieldList);
     }
@@ -68,7 +62,4 @@ const getAllProducts = async (req, res) => {
     res.status(200).json({ products, nbHits: products.length });
 }
 
-module.exports = {
-    getAllProducts,
-    getAllProductsStatic,
-}
+module.exports = { getAllProducts }
